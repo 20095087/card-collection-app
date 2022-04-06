787c6427e9ca8ec10eb2c8f08787c2f962686ff1
ddzik19 <99027550+20095087@users.noreply.github.com>
2022-04-06 18:33:54 +0100

    Update Main.kt
    Added basic crud menu.
    
    This menu will be displayed to the user when the app starts.
    
    Now I need to create a method that will display the menu and ask for input.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 import utils.ScannerInput
 
-fun MainMenu() : Int{
-    return ScannerInput.readNextInt()
+fun main(args: Array<String>) {
+
+}
+
+fun MainMenu(): Int {
+    return ScannerInput.readNextInt(
+        """
+                 > ---------------------------------------
+                 > |        CARD COLLECTION APP          |
+                 > ---------------------------------------
+                 > | MENU                                |
+                 > |   1) Add Album                      |
+                 > |   2) Update Album                   |
+                 > |   3) Delete Album                   |
+                 > |   4) List all Albums                |
+                 > ---------------------------------------
+                 > |   0) Exit                           |
+                 > ---------------------------------------
+    ==>>""".trimMargin(">")
+    )
 }
