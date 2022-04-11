638345ecc073fef996e845134e90b52b7b584d02
ddzik19 <99027550+20095087@users.noreply.github.com>
2022-04-11 17:55:30 +0100

    app main class
    I have set the mainClass of the application to be MainKt.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    application
 }
 
 group = "me.ddzik"
@@ -26,4 +27,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
 }
