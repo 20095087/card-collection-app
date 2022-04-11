8b1ccae0c80803384d9be59812e1b2048962b7e2
ddzik19 <99027550+20095087@users.noreply.github.com>
2022-04-11 18:25:02 +0100

    Update Utilities.kt
    Added utilites class.
    In this class we will hold functions that validate if an index is valid and other functions.

--- src/main/kotlin/utils/Utilities.kt
+++ src/main/kotlin/utils/Utilities.kt
@@ -1,6 +1,11 @@
 package utils
 
 object Utilities {
-
+    // function that will be used to check if the user entered an index
+    // that is within a range
+    @JvmStatic
+    fun validRange(numberToCheck: Int, min: Int, max: Int): Boolean {
+        return numberToCheck in min..max
+    }
 
 }
